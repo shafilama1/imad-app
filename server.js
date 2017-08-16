ca2e53bc1d3fc78b52792fc226b0a53fe73f50e4
shafilama1 <30976535+shafilama1@users.noreply.github.com>
2017-08-17 00:18:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res,send('Article one requested and will be send here');
+    res.send('Article one requested and will be send here');
 });
 
 app.get('/Article-two',function(req,res){
-    res,send('Article two requested and will be send here');
+    res.send('Article two requested and will be send here');
 });
 
 app.get('/Article-three',function(req,res){
-    res,send('Article three requested and will be send here');
+    res.send('Article three requested and will be send here');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
